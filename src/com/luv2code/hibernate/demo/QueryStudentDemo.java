3727cb532ecb9eded3e7a59ad34fc45275c97dff
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-06 07:45:42 +0100

    Hibernate query student table

---
+++ src/com/luv2code/hibernate/demo/QueryStudentDemo.java
@@ -0,0 +1,45 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class QueryStudentDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // query students
+            List<Student > theStudents = session.createQuery("from Student").getResultList();
+
+            // display student
+            for (Student tempStudent: theStudents) {
+                System.out.println(tempStudent);
+            }
+            // query students
+            theStudents = session.createQuery("from Student s where s.lastName = 'hana'").getResultList();
+            // display student
+            for (Student tempStudent: theStudents) {
+                System.out.println(tempStudent);
+            }
+
+            // commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done :)");
+        } finally {
+            session.close();
+        }
+    }
+}
