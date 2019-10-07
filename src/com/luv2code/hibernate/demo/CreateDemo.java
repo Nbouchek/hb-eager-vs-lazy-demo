447f0167f8c16dd166fefb098ac442cfdd6fb7c5
Nacer Bouchekhima <nbouchek@gmail.com>
2019-10-07 07:34:19 +0100

    Hibernate one-to-one relationship demo

--- src/com/luv2code/hibernate/demo/CreateDemo.java
+++ src/com/luv2code/hibernate/demo/CreateDemo.java
@@ -1,35 +1,41 @@
 package com.luv2code.hibernate.demo;
 
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
 import com.luv2code.hibernate.demo.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class PrimaryKeyDemo {
+public class CreateDemo {
     public static void main(String[] args) {
         // create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Student.class)
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
                 .buildSessionFactory();
 
         // create session
         Session session = factory.getCurrentSession();
         try {
-            // create a student object
-            System.out.println("Creating 3 new student objects ... ");
-            Student tempStudent = new Student("Adam", "Hana", "adam@luv2code.com");
-            Student tempStudent1 = new Student("Linda", "Hana", "linda@luv2code.com");
-            Student tempStudent2 = new Student("Daddy", "Hana", "daddy@luv2code.com");
+            // create the objects
+            Instructor tempInstructor = new Instructor("Nacer", "Bouchekhima", "nacer@luv2code.com");
+            InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.ssanabel.com/youtube", "lv 2 code");
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
             // start a transaction
             System.out.println("Starting transaction ... ");
             session.beginTransaction();
 
-            // save the student
-            System.out.println("Saving Students ... ");
-            session.save(tempStudent);
-            session.save(tempStudent1);
-            session.save(tempStudent2);
+            // save the instructor
+            //
+            // Note: this will also save the tempInstructorDetail
+            // because of cascadeType.ALL
+            System.out.println("Saving instructor: " + tempInstructor);
+            session.save(tempInstructor);
 
             // commit transaction
             System.out.println("Committing transaction ... ");
